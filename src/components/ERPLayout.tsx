9ecf06af5c8afc3ada437e907f78baa991ada69c
lamiaahajib37-jpg <haynahayna421@gmail.com>
2026-04-18 15:20:42 +0100

    modif1

--- src/components/ERPLayout.tsx
+++ src/components/ERPLayout.tsx
@@ -1,18 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import { ERPSidebar } from "./ERPSidebar";
 import { ERPTopbar } from "./ERPTopbar";
 
 export function ERPLayout() {
-  const [sidebarOpen, setSidebarOpen] = useState(true);
+  const isMobile = () => window.innerWidth < 768;
+  const [sidebarOpen, setSidebarOpen] = useState(!isMobile());
   const location = useLocation();
+
+  useEffect(() => {
+    if (isMobile()) setSidebarOpen(false);
+  }, [location.pathname]);
+
+  useEffect(() => {
+    const onResize = () => {
+      if (!isMobile()) setSidebarOpen(true);
+    };
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  }, []);
 
   return (
     <div className="flex h-screen overflow-hidden">
-      <ERPSidebar open={sidebarOpen} onToggle={() => setSidebarOpen(!sidebarOpen)} />
-      <div className="flex-1 flex flex-col overflow-hidden">
+      {sidebarOpen && isMobile() && (
+        <div
+          className="fixed inset-0 z-20 bg-black/50"
+          onClick={() => setSidebarOpen(false)}
+        />
+      )}
+      <ERPSidebar
+        open={sidebarOpen}
+        onToggle={() => setSidebarOpen(!sidebarOpen)}
+        isMobile={isMobile()}
+      />
+      <div className="flex-1 flex flex-col overflow-hidden min-w-0">
         <ERPTopbar onToggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
-        <main className="flex-1 overflow-y-auto p-6 fade-in" key={location.pathname}>
+        <main className="flex-1 overflow-y-auto p-4 md:p-6 fade-in" key={location.pathname}>
           <Outlet />
         </main>
       </div>
